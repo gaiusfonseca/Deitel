29d9d5078ca0be74588ca8ce2a5f9e1506fda520
Gaius Fonseca <gaiusfonseca@gmail.com>
2021-09-11 15:59:11 -0300

    exercicios de programacao 2.15

---
+++ Cap02/q2-15/Aritmetics.java
@@ -0,0 +1,27 @@
+/*
+    2.15 Escreva um aplicativo que solicite ao usuário inserir dois inteiros, obtenha dele esses números e imprima sua
+    soma, produto, diferença e quociente (divisão). Utiliza as técnicas mostradas na Figura 2.7.
+*/
+
+import java.util.Scanner;
+
+public class Aritmetics{
+
+    public static void main(String[] args){
+
+        Scanner entrada = new Scanner(System.in);
+        int num1;
+        int num2;
+
+        System.out.print("Por favor, digite um número: ");
+        num1 = entrada.nextInt();
+
+        System.out.print("Por favor, digite outro número: ");
+        num2 = entrada.nextInt();
+
+        System.out.printf("%d + %d = %d%n", num1, num2, num1 + num2);
+        System.out.printf("%d * %d = %d%n", num1, num2, num1 * num2);
+        System.out.printf("%d - %d = %d%n", num1, num2, num1 - num2);
+        System.out.printf("%d / %d = %d%n", num1, num2, num1 / num2);
+    }
+}
