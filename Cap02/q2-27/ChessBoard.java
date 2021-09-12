301703d766af1e17159ce3c37357b1a42e8cf3c5
Gaius Fonseca <gaiusfonseca@gmail.com>
2021-09-11 22:29:47 -0300

    exercicios de programacao 2.27

---
+++ Cap02/q2-27/ChessBoard.java
@@ -0,0 +1,38 @@
+/*
+    Escreva um aplicativo que exiba um padrão de tabuleiro de damas, como mostrado a seguir:
+    '* * * * * * * * '
+    ' * * * * * * * *'
+    '* * * * * * * * '
+    ' * * * * * * * *'
+    '* * * * * * * * '
+    ' * * * * * * * *'
+    '* * * * * * * * '
+    ' * * * * * * * *'
+*/
+
+public class ChessBoard{
+
+    public static void main(String[] args){
+
+        /*  
+            Este problema é na realidade muito simples, notem que se trata de apenas dois padrões que se repetem de forma
+            alternada.
+         */
+
+        //primeiro conjunto
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * *");
+
+        //segundo conjunto
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * *");
+
+        //terceiro conjunto
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * *");
+
+        //quarto conjunto
+        System.out.println("* * * * * * * * ");
+        System.out.println(" * * * * * * * *");
+    }
+}
