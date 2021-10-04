a672a7dfca1ae7d74d4e1de2889daf5e93d8cdfc
Gaius Fonseca <gaiusfonseca@gmail.com>
2021-10-04 01:15:05 -0300

    exercicios de programacao 7.15

---
+++ Cap07/q7-15/App.java
@@ -0,0 +1,28 @@
+/* 
+    7.15 (Argumentos de linha de comando) Reescreva a Figura 7.2 para que o tamanho do array seja especificado pelo 
+    primeiro argumento de linha de comando. Se nenhum argumento de linha de comando for fornecido, utilize 10 como o 
+    tamanho padrão do array.
+*/
+
+public class App{
+
+    public static void main(String[] args) {
+        
+        int size = 10;
+        int[] array;
+
+        if(args.length == 1){
+            
+            size = Integer.parseInt(args[0]);
+            array = new int[size];
+        }else{
+            array = new int[size];
+        }
+
+        System.out.printf("%s%8s%n", "Index", "Values");
+
+        for(int counter = 0; counter < array.length; counter++){
+            System.out.printf("%5d%8d%n", counter, array[counter]);
+        }
+    }
+}
