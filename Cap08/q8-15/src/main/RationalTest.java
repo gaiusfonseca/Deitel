98dd2354843c2d5239e4dbf0b7c7160807635fd7
Gaius Fonseca <gaiusfonseca@gmail.com>
2022-10-23 21:28:49 -0300

    escrevendo teste para lançar uma IllegalArgumentException quando um dos argumentos for negativo

--- Cap08/q8-15/src/main/RationalTest.java
+++ Cap08/q8-15/src/main/RationalTest.java
@@ -5,16 +5,42 @@ public class RationalTest {
 	public static void main(String[] args) {
 
 		shouldThrowEceptionWhenDenominatorIsZero();
+		shouldThrowEceptionWhenAnyArgumentIsNegative();
 		shouldSucceedToCreateARational();
 	}
 	
 	public static void shouldThrowEceptionWhenDenominatorIsZero() {
 		System.out.print("Tenta criar um Rational com denominador 0: ");
+		
 		try {
 			Rational myNum = new Rational(12, 0);
 		}catch(ArithmeticException e) {
 			System.out.printf("passou. falhou com a mensagem: %s%n", e.getMessage());
 		}
+		
+		System.out.print("não passou.\n");
+	}
+	
+	public static void shouldThrowEceptionWhenAnyArgumentIsNegative() {
+		System.out.print("Tenta criar um Rational com denominador -1: ");
+		
+		try {
+			Rational myNum = new Rational(12, -1);
+		}catch(IllegalArgumentException e) {
+			System.out.printf("passou. falhou com a mensagem: %s%n", e.getMessage());
+		}
+		
+		System.out.print("não passou.\n");
+		
+		System.out.print("Tenta criar um Rational com numerator -1: ");
+		
+		try {
+			Rational myNum = new Rational(-1, 12);
+		}catch(IllegalArgumentException e) {
+			System.out.printf("passou. falhou com a mensagem: %s%n", e.getMessage());
+		}
+		
+		System.out.print("não passou.\n");
 	}
 	
 	public static void shouldSucceedToCreateARational() {
