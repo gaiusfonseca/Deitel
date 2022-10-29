302f2d635c3efeece5420b308363d3f5e67b9e7c
Gaius Fonseca <gaiusfonseca@gmail.com>
2022-10-28 23:24:59 -0300

    implementação do teste do método isEqualTo

--- Cap08/q8-16/src/main/HugeIntegerTest.java
+++ Cap08/q8-16/src/main/HugeIntegerTest.java
@@ -1,8 +1,38 @@
 package main;
 
-public class HugeIntegerTest {
+import java.util.Scanner;
 
+public class HugeIntegerTest {
+	
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int size = 40;
+		int[] numberFromString = new int[size];
+		
+		System.out.println("Cria um HugeInteger com o construtor padrão");
+		HugeInteger number = new HugeInteger();
+		
+		number.showValue();
+		
+		System.out.print("Informe um número com até 40 caracteres: ");
+		if(input.hasNext()) {
+			numberFromString = HugeInteger.parse(input.next());
+			number.setNumber(numberFromString);
+			number.showValue();
+		}
+		
+		System.out.println("cria um novo array com o valor 100 e verifica se ele é igual a 'number'.\n");
+		HugeInteger number2 = new HugeInteger();
+		number2.setNumber(numberFromString);
+		if(number2.isEqualTo(number)) {
+			System.out.println("os arrays são iguais.");
+			System.out.printf("number: %s%n", number.toString());
+			System.out.printf("number2: %s%n", number2.toString());
+		}else {
+			System.out.println("os arrays não são iguais.");
+			System.out.printf("number: %s%n", number.toString());
+			System.out.printf("number2: %s%n", number2.toString());
+		}
 		
 	}
 }
